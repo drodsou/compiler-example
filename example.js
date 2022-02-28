a7d64323d8de95a11dba1e50497facc8fbfb10ee
drodsou <gitman@drodsou.com>
2022-02-28 21:56:16 +0100

    setvar

--- example.js
+++ example.js
@@ -2,9 +2,24 @@ import {tokenizer, parser, compiler} from './compiler.js';
 import lang from './langDef1.js';
 
 
-const src = `
+/*
   (add 4 (add 2 3))
   (mult 3 2 10)
+  
+  
+
+  (setvar "v1" 1)
+  (loop 2 
+    (setvar "v1" 
+      (add (getvar "v1") 1)
+    )
+  )
+  (getvar "v1")
+*/
+
+const src = `
+  (setvar "v1" (add 2 3))
+  (mult 2 (getvar "v1"))
 `
 
 const tokens = tokenizer(src);
--- langDef1.js
+++ langDef1.js
@@ -1,9 +1,14 @@
+// -- language runtime vars
+const runtime = {
+  
+}
+
 // -- our language valid functions
 const lang = {
   
   add : (args)=>{
-    // syntax
-    console.log('executing add', args);
+    // semantic check
+    console.log('\nexecuting add', args);
     if (args.find(a=>a.type !== 'NumberLiteral')) {
       throw new Error('add: all parameters must be numbers');
     }
@@ -15,8 +20,8 @@ const lang = {
 
 
   mult : (args)=>{
-    // syntax
-    console.log('executing mult', args);
+    // semantic check
+    console.log('\nexecuting mult', args);
     if (args.find(a=>a.type !== 'NumberLiteral')) {
       throw new Error('mult: all parameters must be numbers');
     }
@@ -25,6 +30,32 @@ const lang = {
     let calc = args.reduce( (p,c)=>p * parseInt(c.value),1)
     return {type: 'NumberLiteral', value: calc.toString()}
   },
+
+  
+  loop : (args)=>{
+    console.log('\nexecuting loop', args);
+    // todo: semantic check
+    let ret = []
+    for (let n = 1; n <= parseInt(args[0].value); n++) {
+      // TODO: call compiler here?
+      ret.push({type: 'StringLiteral', value:args[1]})
+    }
+    return ret
+  },
+
+  setvar : (args)=>{
+    console.log('executing setvar', args);
+    // todo: semantic check
+    runtime[args[0].value] = parseInt(args[1].value)
+    return {}
+  },
+
+  getvar : (args)=>{
+    console.log('executing getvar', args);
+    // todo: semantic check
+    return {type: 'NumberLiteral', value: runtime[args[0].value] }
+  }
+
 }
 
 export default lang
